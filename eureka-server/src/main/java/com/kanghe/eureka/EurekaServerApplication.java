31e394497ac7a77a346ea2d60c06ce70246c0762
wujun <18813918091@163.com>
2019-04-10 12:48:47 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/kanghe/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.kanghe.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: W_jun1
+ * @Date: 2019/4/10 10:34
+ * @Description: 注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
