e0a967580c63ed66af72b95f72b1a65deae880bc
wujun <18813918091@163.com>
2019-04-13 17:15:56 +0800

    Initial commit

--- service-member-manage/src/main/java/com/kanghe/service/member/exception/GlobalExceptionHandler.java
+++ service-member-manage/src/main/java/com/kanghe/service/member/exception/GlobalExceptionHandler.java
@@ -3,9 +3,15 @@ package com.kanghe.service.member.exception;
 import com.kanghe.component.common.base.BaseResult;
 import com.kanghe.component.common.enums.ResultEnum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @Auther: W_jun1
@@ -40,6 +46,28 @@ public class GlobalExceptionHandler {
     public BaseResult handleBuzException(MemberServiceException e) {
         log.info("业务异常: {}", e.getMessage());
         return new BaseResult(Boolean.TRUE, e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 请求参数校验
+     *
+     * @param e
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseResult validationBodyException(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        if (result.hasErrors()) {
+            List<ObjectError> errors = result.getAllErrors();
+            for (ObjectError p : errors) {
+                FieldError fieldError = (FieldError) p;
+                String thisMsg = fieldError.getDefaultMessage();
+                log.info("请求参数校验错误: name={}, field={}, msg={}", fieldError.getObjectName(), fieldError.getField(), thisMsg);
+                return new BaseResult(Boolean.FALSE, ResultEnum.INVALID_PARAM.getCode(), e.getMessage());
+            }
+        }
+        return new BaseResult(Boolean.FALSE, ResultEnum.INVALID_PARAM.getCode(), e.getMessage());
     }
 
 }
