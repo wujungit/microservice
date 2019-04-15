8529f4570135e5b63aa186e2a827a7016ece9303
wujun <18813918091@163.com>
2019-04-15 10:38:35 +0800

    Initial commit

---
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/controller/Test.java
@@ -0,0 +1,9 @@
+package com.kanghe.component.rocketmq.controller;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/4/15 10:38
+ * @Description:
+ */
+public class Test {
+}
---
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/threadpool/Test.java
@@ -0,0 +1,9 @@
+package com.kanghe.component.rocketmq.threadpool;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/4/15 10:37
+ * @Description:
+ */
+public class Test {
+}
