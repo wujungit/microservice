c7508733b6f69d00c621529bbf6061b6698df974
wujun <18813918091@163.com>
2019-04-15 19:26:53 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/controller/TestController.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.kanghe.component.rocketmq.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author: W_jun1
  * @Date: 2019/4/15 16:17
  * @Description:
  */
+@RestController
 public class TestController {
+
+
+
 }
