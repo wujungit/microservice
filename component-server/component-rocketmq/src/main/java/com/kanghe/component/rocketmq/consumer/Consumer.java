c5cd022beabf0486abcb5eff6e2e4e925ca18286
wujun <18813918091@163.com>
2019-04-16 14:50:05 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/consumer/Consumer.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/consumer/Consumer.java
@@ -5,9 +5,13 @@ import com.kanghe.component.rocketmq.config.ConsumerConfig;
 import com.kanghe.component.rocketmq.exception.RocketMQException;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,37 +20,42 @@ import org.springframework.stereotype.Component;
  * @Description:
  */
 @Component
-public class Consumer {
+public class Consumer implements CommandLineRunner {
 
     @Autowired
     private ConsumerConfig consumerConfig;
 
-    @Bean
-    public DefaultMQPushConsumer getConsumer() {
+    @Override
+    public void run(String... args) throws Exception {
+        messageListener();
+    }
+
+    private void messageListener() {
         if (StringUtils.isBlank(consumerConfig.getGroupName())) {
             throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "groupName is blank");
         }
         if (StringUtils.isBlank(consumerConfig.getNamesrvAddr())) {
             throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "namesrvAddr is blank");
         }
-//        if (StringUtils.isBlank(consumerConfig.getTopics())) {
-//            throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "topics is blank");
-//        }
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(consumerConfig.getGroupName());
         consumer.setNamesrvAddr(consumerConfig.getNamesrvAddr());
-//        consumer.setConsumeThreadMin(consumerConfig.getConsumeThreadMin());
-//        consumer.setConsumeThreadMax(consumerConfig.getConsumeThreadMax());
-//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
-//        consumer.setConsumeMessageBatchMaxSize(consumerConfig.getConsumeMessageBatchMaxSize());
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
 
         try {
+            //在此监听中消费信息，并返回消费的状态信息
+            consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+                // 会把不同的消息分别放置到不同的队列中
+                for (Message msg : msgs) {
+                    System.out.println("接收到了消息：" + new String(msg.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+
+            });
             consumer.start();
         } catch (MQClientException e) {
             e.printStackTrace();
             throw new RocketMQException(e.getResponseCode(), e.getErrorMessage());
         }
-
-        return consumer;
     }
 
 }
--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/producer/Producer.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/producer/Producer.java
@@ -1,9 +1,6 @@
 package com.kanghe.component.rocketmq.producer;
 
-import com.kanghe.component.common.enums.ResultEnum;
 import com.kanghe.component.rocketmq.config.ProducerConfig;
-import com.kanghe.component.rocketmq.exception.RocketMQException;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
@@ -13,9 +10,9 @@ import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -31,34 +28,19 @@ public class Producer {
 
     private DefaultMQProducer producer;
 
-    @Bean
-    public DefaultMQProducer getProducer() {
-        if (StringUtils.isBlank(producerConfig.getGroupName())) {
-            throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "groupName is blank");
-        }
-        if (StringUtils.isBlank(producerConfig.getNamesrvAddr())) {
-            throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "nameServerAddr is blank");
-        }
-        DefaultMQProducer producer = new DefaultMQProducer(producerConfig.getGroupName());
+    @PostConstruct
+    public void defaultMQProducer() {
+        //生产者的组名
+        producer = new DefaultMQProducer(producerConfig.getGroupName());
+        //指定NameServer地址，多个地址以;隔开
         producer.setNamesrvAddr(producerConfig.getNamesrvAddr());
-//        if (producerConfig.getMaxMessageSize() != null) {
-//            producer.setMaxMessageSize(producerConfig.getMaxMessageSize());
-//        }
-//        if (producerConfig.getSendMsgTimeout() != null) {
-//            producer.setSendMsgTimeout(producerConfig.getSendMsgTimeout());
-//        }
-//        if (producerConfig.getRetryTimesWhenSendFailed() != null) {
-//            producer.setRetryTimesWhenSendFailed(producerConfig.getRetryTimesWhenSendFailed());
-//        }
-
+        producer.setVipChannelEnabled(false);
         try {
             producer.start();
+            System.out.println("-------->:producer启动了");
         } catch (MQClientException e) {
             e.printStackTrace();
-            throw new RocketMQException(e.getResponseCode(), e.getErrorMessage());
         }
-
-        return producer;
     }
 
     public String send(String topic, String tags, String body) throws InterruptedException, RemotingException, MQClientException, MQBrokerException, UnsupportedEncodingException {
