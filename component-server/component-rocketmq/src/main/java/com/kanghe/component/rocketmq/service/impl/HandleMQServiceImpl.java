0603a5e0a4a5628226121b8006dc247859a76d84
wujun <18813918091@163.com>
2019-04-25 13:33:46 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/service/impl/HandleMQServiceImpl.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/service/impl/HandleMQServiceImpl.java
@@ -1,11 +1,17 @@
 package com.kanghe.component.rocketmq.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.kanghe.component.common.enums.ResultEnum;
+import com.kanghe.component.rocketmq.config.ConsumerConfig;
+import com.kanghe.component.rocketmq.exception.RocketMQException;
 import com.kanghe.component.rocketmq.service.IHandleMQService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,18 +29,34 @@ import java.util.List;
 public class HandleMQServiceImpl implements IHandleMQService {
 
     @Autowired
-    private DefaultMQPushConsumer consumer;
+    private ConsumerConfig consumerConfig;
 
     @Override
     public Message handle() {
-        List<Message> list = new ArrayList<>();
-        //在此监听中消费信息，并返回消费的状态信息
-        consumer.registerMessageListener((MessageListenerConcurrently) (messages, consumeConcurrentlyContext) -> {
-            list.addAll(messages);
-            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-        });
-        log.info("list: {}", JSON.toJSONString(list));
-        return list.isEmpty() ? new Message() : list.get(0);
+        try {
+            if (StringUtils.isBlank(consumerConfig.getGroupName())) {
+                throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "groupName is blank");
+            }
+            if (StringUtils.isBlank(consumerConfig.getNamesrvAddr())) {
+                throw new RocketMQException(ResultEnum.INVALID_PARAM.getCode(), "namesrvAddr is blank");
+            }
+            DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(consumerConfig.getGroupName());
+            consumer.setNamesrvAddr(consumerConfig.getNamesrvAddr());
+            consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+            consumer.subscribe("PushTopic", "");
+
+            //在此监听中消费信息，并返回消费的状态信息
+            List<Message> list = new ArrayList<>();
+            consumer.registerMessageListener((MessageListenerConcurrently) (messages, consumeConcurrentlyContext) -> {
+                list.addAll(messages);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            });
+            log.info("list: {}", JSON.toJSONString(list));
+            return list.isEmpty() ? new Message() : list.get(0);
+        } catch (MQClientException e) {
+            e.printStackTrace();
+            throw new RocketMQException(ResultEnum.MQ_EXECUTE_ERROR.getCode(), ResultEnum.MQ_EXECUTE_ERROR.getMsg());
+        }
     }
 
 }
