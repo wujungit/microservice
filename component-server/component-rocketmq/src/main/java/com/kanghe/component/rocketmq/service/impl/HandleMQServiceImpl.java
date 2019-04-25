f2694015bbbf2bb463df7d372b7f1612a1d8b461
wujun <18813918091@163.com>
2019-04-25 15:19:11 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/service/impl/HandleMQServiceImpl.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/service/impl/HandleMQServiceImpl.java
@@ -1,23 +1,12 @@
 package com.kanghe.component.rocketmq.service.impl;
 
-import com.alibaba.fastjson.JSON;
 import com.kanghe.component.common.enums.ResultEnum;
-import com.kanghe.component.rocketmq.config.ConsumerConfig;
 import com.kanghe.component.rocketmq.exception.RocketMQException;
 import com.kanghe.component.rocketmq.service.IHandleMQService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static com.kanghe.component.rocketmq.consumer.Consumer.MQ_CONTAINER;
@@ -31,14 +20,11 @@ import static com.kanghe.component.rocketmq.consumer.Consumer.MQ_CONTAINER;
 @Slf4j
 public class HandleMQServiceImpl implements IHandleMQService {
 
-    @Autowired
-    private ConsumerConfig consumerConfig;
-
     @Override
     public Message handle() {
         try {
             ConcurrentLinkedQueue<Message> queue = MQ_CONTAINER.get("PushTopic");
-            queue.peek();
+            return queue.peek();
         } catch (Exception e) {
             e.printStackTrace();
             throw new RocketMQException(ResultEnum.MQ_EXECUTE_ERROR.getCode(), ResultEnum.MQ_EXECUTE_ERROR.getMsg());
