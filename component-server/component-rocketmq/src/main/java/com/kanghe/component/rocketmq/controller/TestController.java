01789daa1963dbeff8226f7ceb09c2079b3b300e
wujun <18813918091@163.com>
2019-04-26 17:20:26 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/controller/TestController.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/controller/TestController.java
@@ -1,14 +1,11 @@
 package com.kanghe.component.rocketmq.controller;
 
-import com.kanghe.component.rocketmq.service.IHandleMQService;
+import com.kanghe.component.rocketmq.annotation.MQListener;
 import com.kanghe.component.rocketmq.service.ISendMQService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: W_jun1
@@ -25,18 +22,15 @@ public class TestController {
 
     @Autowired
     private ISendMQService sendMQService;
-    @Autowired
-    private IHandleMQService handleMQService;
 
     @GetMapping("/push")
     public String pushMsg(@RequestParam String msg) {
         return sendMQService.send(TOPIC, TAG, msg);
     }
 
-//    @MQListener(topic = TOPIC, tag = TAG)
+    @MQListener(topic = TOPIC, tag = TAG)
     @GetMapping("/pull")
-    public void pullMsg() {
-        Message msg = handleMQService.handle();
+    public void pullMsg(@RequestBody Message msg) {
         if (null != msg) {
             byte[] body = msg.getBody();
             if (null != body && body.length > 0) {
