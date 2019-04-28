ed6874eba1114d1f805621643d4f0df934a8ddc3
W_jun1 <18813918091@163.com>
2019-04-28 23:53:47 +0800

    Initial commit

--- service-order-manage/src/main/java/com/kanghe/service/order/OrderServiceApplication.java
+++ service-order-manage/src/main/java/com/kanghe/service/order/OrderServiceApplication.java
@@ -1,7 +1,9 @@
 package com.kanghe.service.order;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
