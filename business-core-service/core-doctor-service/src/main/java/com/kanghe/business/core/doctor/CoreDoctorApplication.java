53ec631b1b7d3ee6118a843bc595e90e62bd1189
wujun <18813918091@163.com>
2019-04-28 17:01:50 +0800

    Initial commit

--- business-core-service/core-doctor-service/src/main/java/com/kanghe/business/core/doctor/CoreDoctorApplication.java
+++ business-core-service/core-doctor-service/src/main/java/com/kanghe/business/core/doctor/CoreDoctorApplication.java
@@ -1,9 +1,18 @@
 package com.kanghe.business.core.doctor;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * @Auther: W_jun1
  * @Date: 2019/4/12 09:48
  * @Description:
  */
+@SpringBootApplication
 public class CoreDoctorApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(CoreDoctorApplication.class).run(args);
+    }
+
 }
--- business-core-service/core-member-service/src/main/java/com/kanghe/business/core/patient/CoreMemberApplication.java
+++ business-core-service/core-member-service/src/main/java/com/kanghe/business/core/patient/CoreMemberApplication.java
@@ -1,9 +1,18 @@
 package com.kanghe.business.core.patient;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * @Auther: W_jun1
  * @Date: 2019/4/12 09:49
  * @Description:
  */
+@SpringBootApplication
 public class CoreMemberApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(CoreMemberApplication.class).run(args);
+    }
+
 }
