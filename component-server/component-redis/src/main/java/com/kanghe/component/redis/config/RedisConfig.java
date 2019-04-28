8d4bf95c7935221e0a74abef4f9ad473de944c33
wujun <18813918091@163.com>
2019-04-28 16:55:06 +0800

    Initial commit

--- component-server/component-redis/src/main/java/com/kanghe/component/redis/config/RedisConfig.java
+++ component-server/component-redis/src/main/java/com/kanghe/component/redis/config/RedisConfig.java
@@ -35,7 +35,7 @@ public class RedisConfig extends CachingConfigurerSupport {
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
         template.setConnectionFactory(factory);
-        // key序列化方式
+        // key序列化
         template.setKeySerializer(jackson2JsonRedisSerializer);
         // value序列化
         template.setValueSerializer(jackson2JsonRedisSerializer);
@@ -49,7 +49,7 @@ public class RedisConfig extends CachingConfigurerSupport {
     public CacheManager cacheManager(RedisConnectionFactory factory) {
         RedisSerializer<String> redisSerializer = new StringRedisSerializer();
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        //解决查询缓存转换异常的问题
+        // 解决查询缓存转换异常的问题
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
