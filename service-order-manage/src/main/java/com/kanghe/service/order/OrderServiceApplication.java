00248ff68e3f065f256243db4288dda384bf0682
wujun <18813918091@163.com>
2019-04-30 10:33:10 +0800

    Initial commit

--- service-order-manage/src/main/java/com/kanghe/service/order/OrderServiceApplication.java
+++ service-order-manage/src/main/java/com/kanghe/service/order/OrderServiceApplication.java
@@ -3,7 +3,6 @@ package com.kanghe.service.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -14,7 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableSwagger2
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kanghe.component.rocketmq"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
