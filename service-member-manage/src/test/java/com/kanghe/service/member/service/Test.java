fb8fe48cb03a534b4578325dc824ff0ad1834f4b
wujun <18813918091@163.com>
2019-04-30 17:01:01 +0800

    Initial commit

---
+++ service-member-manage/src/test/java/com/kanghe/service/member/service/Test.java
@@ -0,0 +1,9 @@
+package com.kanghe.service.member.service;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/4/30 16:53
+ * @Description:
+ */
+public class Test {
+}
---
+++ service-order-manage/src/test/java/com/kanghe/service/order/controller/Test.java
@@ -0,0 +1,9 @@
+package com.kanghe.service.order.controller;
+
+/**
+ * @Author: W_jun1
+ * @Date: 2019/4/30 16:55
+ * @Description:
+ */
+public class Test {
+}
