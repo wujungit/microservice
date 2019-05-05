96483ccc51cf7d9fd6c28b600f201b341edafa30
wujun <18813918091@163.com>
2019-05-05 16:39:45 +0800

    Initial commit

--- component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/RocketMQApplication.java
+++ component-server/component-rocketmq/src/main/java/com/kanghe/component/rocketmq/RocketMQApplication.java
@@ -10,9 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class RocketMQApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RocketMQApplication.class, args);
     }
-
 }
